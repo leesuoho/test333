9bb9b37478dbfb1a3f4a8e1fa2f5fecdf4cc2d1e
flashback7 <97232474+seoho1@users.noreply.github.com>
2024-11-19 22:28:11 +0900

    2를 3번 추가함

--- src/ProductList.java
+++ src/ProductList.java
@@ -1,5 +1,8 @@
 public class ProductList {
     public static void main(String[] args) {
         System.out.println();
+        System.out.println(2);
+        System.out.println(2);
+        System.out.println(2);
     }
 }
